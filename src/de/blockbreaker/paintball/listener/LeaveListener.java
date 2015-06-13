8d48fa04a7950dd74355b49f51d7fea551755ea8
Blackagent <ja7n7ne@gmx.de>
2015-06-13 20:47:42 +0200

    Paintball

--- src/de/blockbreaker/paintball/listener/LeaveListener.java
+++ src/de/blockbreaker/paintball/listener/LeaveListener.java
@@ -4,12 +4,13 @@ import de.blockbreaker.paintball.data.Data;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  * Created by Janne on 01.06.2015.
  */
-public class LeaveListener {
+public class LeaveListener implements Listener{
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
